d5e1b17d4d49d11f185917db8faa9b7d0ea01c4c
Merc <mc.hausler@hotmail.com>
2019-05-15 16:04:45 +0200

    addedAllFiles

---
+++ WEBDEV/WEBDEV/bin/Debug/AppX/js/main.js
@@ -0,0 +1,11 @@
+﻿function startGame() {
+    let elem = document.querySelector('.pulse');
+    let animation = elem.animate({
+        opacity: [0.5, 1],
+        transform: ['scale(0.5)', 'scale(1)'],
+    }, {
+            direction: 'alternate',
+            duration: 500,
+            iterations: Infinity,
+        });
+}
